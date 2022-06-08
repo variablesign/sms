913a3cd7f9cf31e4beb71642a383af06779b5e60
Variable Sign <variablesign@gmail.com>
2022-06-08 10:08:35 +0000

    fix: update return value

--- src/Facades/Sms.php
+++ src/Facades/Sms.php
@@ -9,8 +9,8 @@ use Illuminate\Support\Facades\Facade;
  * @method static \VariableSign\Sms\Sms to(array|string $recipients)
  * @method static \VariableSign\Sms\Sms message(string $message)
  * @method static int balance()
- * @method static null|array send()
- * @method static null|array report(string|int $id)
+ * @method static \Illuminate\Support\Collection send()
+ * @method static \Illuminate\Support\Collection report(string|int $id)
  * 
  * @see \VariableSign\Sms\Sms
  */
